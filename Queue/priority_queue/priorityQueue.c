17df48d0d4d5c491736b9ebdcb44b901b60c1b9c
Tanbir <tanbirka@thoughtworks.com>
2013-12-14 10:43:53 +0530

    done reusing circular queue lib

---
+++ Queue/priority_queue/priorityQueue.c
@@ -0,0 +1,37 @@
+#include "priorityQueue.h"
+#include <memory.h>
+#include <stdlib.h>
+int compareData ( void* a,  void* b){
+    return (*(int*)a - *(int*)b);
+}
+
+
+int Penqueue(Queue* queue, void* element,compare comp){
+    int j = (queue->rear)-1;
+    void* elementToCompare;
+    int comparisonResult;
+    if(queueIsFull(queue))
+        return 0;
+    if(queue->rear >= queue->length)
+    	queue->rear = 0;
+    if(queue->front == -1 && queue->rear == -1){
+    	queue->front++;
+    	queue->rear++;
+    }
+    else{
+        queue->rear++;
+        for (j = queue->rear-1; j >= queue->front; j--) {
+            elementToCompare = queue->base + j * queue->elementSize;
+            comparisonResult = comp(element, elementToCompare);
+            if (comparisonResult >= 0) {
+                break; 
+            }
+            memmove(elementToCompare + queue->elementSize, 
+                    elementToCompare, queue->elementSize);                                     
+        } 
+        memmove(queue->base+((j+1)*queue->elementSize), element, queue->elementSize);
+        return 1;   
+    }
+    memmove(queue->base+(queue->rear*queue->elementSize), element, queue->elementSize);
+    return 1;
+}
